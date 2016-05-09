d8fbad8629a777124e428d5242a8c5f7ad5e4c88
ThomasBourany <thomas.bourany@gmail.com>
2016-05-09 18:56:52 +0200

    Add files
    Add files in a « source folder », One for the model and the « shooting
    algorithm » and the other for the different shocks from fiscal policy.
    This second file (« policy experiment ») need to be improved.

---
+++ src/FiscalPolicyModel.jl
@@ -0,0 +1,451 @@
+
+
+module FiscalPolicyModel
+
+
+using NLopt, PyPlot
+using Roots, Distributions
+
+function Parameter()
+# share of capital in the production function
+a = 0.33
+#productivity
+z = 1.0
+# depreciation
+d = 0.2
+# discount factor
+b = 0.95
+rho = (1.0/b) - 1.0
+# CRRA
+sig = 2
+# initial gvt exp (calib for US)
+g = 0.2
+# number of period needed to come back to the Steady State
+# Therefore the period 1 is the initial condition, and period S+2 is the steady state
+S = 30
+## Range of value for endogenous variable
+rangelb = 1e-6
+rangeub = 100
+return Dict("alpha"=>a, "z"=>z, "beta"=>b, "rho"=> rho, "sigma" => sig, "delta"=> d, "S"=>S, "ub"=> rangeub, "lb"=> rangelb)
+end
+
+function u(c, p::Dict) ##Utility
+### Use the CRRA Utility fct
+co=float(c)
+sig=float(p["sigma"])
+if sig >1.0 
+return ((co^(1-sig))-1)/(1-sig)
+elseif sig == 1.0
+return ln(co)
+end
+end
+
+function deru(c, p::Dict) ##Utility
+### Use the CRRA Utility fct
+co=float(c)
+sig=float(p["sigma"])
+if sig >1.0
+return co^(-sig)
+elseif sig == 1.0
+return 1/co
+end
+end
+
+function invderu(u, p::Dict)
+uo=float(u)
+sig=float(p["sigma"])
+if sig>1.0
+return uo^(-1/sig)
+elseif sig == 1.0
+return 1/uo
+end 
+end
+
+function f(k::Real,p::Dict) #Production
+### Cobb Douglas
+alpha = float(p["alpha"])
+ko = float(k)
+return ko^alpha
+end
+
+function derf(k::Real,p::Dict)
+alpha = float(p["alpha"])
+ko = float(k)
+return alpha*ko^(alpha-1)
+end
+
+type Exovar
+	## exogenous policy vector
+	g::Vector{Real}		#public spending
+	tauk::Vector{Real}	#tax on capital
+	tauc::Vector{Real}	#tax on consumption
+	taun::Vector{Real}	#tax on labor
+end
+
+type Endovar
+	c::Vector{Real} #conso
+	k::Vector{Real}	#capital
+	q::Vector{Real}	#pretax price of the good
+	eta::Vector{Real}	#pretax profit of HH
+	w::Vector{Real}		#pretax wage
+	R::Vector{Real}		#gross one-period interest rate
+	r::Vector{Real}		#net interest rate
+end
+
+function exovariable(var1::Vector,var2::Vector,var3::Vector, var4::Vector, p::Dict)
+## Function gathering the fluctuation of the exogenous variable until their last period (var[end])
+## Add enough time at the end until the steady state value, (for the endogenous variable to adjust)
+var1bar=var1[end]*ones(p["S"])
+var2bar=var2[end]*ones(p["S"])
+var3bar=var3[end]*ones(p["S"])
+var4bar=var4[end]*ones(p["S"])
+cvar1 = append!(var1,var1bar)
+cvar2 = append!(var2,var2bar)
+cvar3 = append!(var3,var3bar)
+cvar4 = append!(var4,var4bar)
+return Exovar(cvar1, cvar2, cvar3, cvar4)
+end
+
+
+
+function endovariable(exovar::Exovar)
+## generate endogenous variable, empty, to be used latter in the shooting algorithm
+n = length(exovar.g)
+c = zeros(n)
+k = zeros(n)
+q = zeros(n)
+eta= zeros(n)
+w= zeros(n)
+R= zeros(n)
+r= zeros(n)
+return Endovar(c,k,q,eta,w,R,r)
+end
+
+type Model
+	exo::Exovar
+	endo::Endovar
+	u::Function
+	deru::Function
+	invderu::Function
+	f::Function
+	derf::Function
+	p::Dict
+	n::Int ## number of period in the model, including the S (adjustment)
+end
+
+function Imp_model(exo::Exovar, endo::Endovar, u::Function, deru::Function, invderu::Function, f::Function, derf::Function, p::Dict)
+	n = length(exo.g)
+	return Model(exo, endo, u, deru,invderu, f, derf, p, n)
+end
+
+## All the following functions are part of the "final" shooting algorithm. That is why we start 
+## from the computed value of the steady state. After that will choose (in a "rule of thumb" manner) the initial value of the control variable
+## to approximate this steady state value.  
+
+function steadystate(m::Model)
+## We compute the steady state value of the capital stock
+taukbar = m.exo.tauk[end]
+sol(k) = m.p["delta"] + (m.p["rho"]/(1-taukbar)) - m.derf(k,m.p)
+kbar = fzero(k->sol(k),[m.p["lb"],m.p["ub"]])
+## The kbar satisfy the equation XX of the paper. 
+## We assign this steady state value to the vector of endogenous variables
+m.endo.k[end] = kbar
+return nothing
+end 
+
+
+function lawofmotion_k(m::Model, c::Vector, k::Vector, i) ##Compute next period capital 
+g=m.exo.g
+ki= m.f(k[i],m.p) + ((1-m.p["delta"])*k[i]) - g[i] - c[i]
+if ki>0
+k[i+1]=ki
+elseif ki<=0
+k[i+1]=0.0
+end
+return nothing
+end
+
+
+function eulerequation(m::Model, uc::Vector, k::Vector, i) ##Compute next period marg.utility
+tauc=m.exo.tauc
+tauk=m.exo.tauk
+sol(uprime) = (uprime*(m.p["beta"]*((1+tauc[i])/(1+tauc[i+1]))*((1-tauk[i+1])*(m.derf(k[i+1],m.p) - m.p["delta"]) + 1))) - uc[i]
+
+## solving for the next period marg utility
+ui=fzeros(u->sol(u), [-Inf, Inf])
+ui_bis =[ui;NaN] # in case the euler equation is always infinite and the fzeros gives a 0-element array
+uc[i+1]=ui_bis[1]
+return nothing
+end
+
+
+function equilibriumquantities(m::Model, c::Vector, k::Vector) ## From the path of consumption and capital, compute the other endogenous variables
+
+## As preliminary assign the newly computed c and k quantities (in fact it does not matter when you do it.)
+m.endo.c = c
+m.endo.k = k
+
+tc = m.exo.tauc # rename conso-tax for simplicity
+tk = m.exo.tauk
+
+#Loop
+for i in 1:m.n
+## First consumption (even though we already computed it in the loop)
+#if i >1
+#m.endo.c[i-1] = f(k[i-1],m.p) + (1-m.p["delta"])*(k[i-1]) - k[i] - m.exo.g[i-1]
+#end 
+#if i=m.n
+#m.endo.c[m.n] = f(k[m.n],m.p) + (1-m.p["delta"])*(k[m.n]) - m.exo.g[m.n]
+#end
+
+## Second compute the sequence of asset prices
+m.endo.q[i]=m.p["beta"]^i /(1+tc[i])
+
+## Third compute the rental rate of HH from capital
+m.endo.eta[i]= m.derf(m.endo.k[i],m.p)
+
+## Fourth, the wage 
+m.endo.w[i]= m.f(k[i],m.p)- k[i] * m.derf(k[i],m.p)
+
+## Fifth the gross interest rate btw t and t+1
+if i >1
+m.endo.R[i-1]= ((1-tk[i])*(m.derf(k[i],m.p) - m.p["delta"]) +1)
+end 
+
+if i==m.n
+m.endo.R[m.n]=m.endo.R[m.n-1] 
+end
+
+## Sixth the net interest rate
+if i >1
+m.endo.r[i-1]= m.endo.R[i-1]-1
+end 
+if i==m.n
+m.endo.r[m.n]=m.endo.r[m.n-1] 
+end
+
+end 
+## setting the last interest rate as equal to the one before the last (at steady-state, we suppose it does not fluctuate much). 
+
+return nothing
+end 
+
+
+
+function loop_c_k(m::Model, c0::Real, kbar::Real)
+
+k_temp = zeros(m.n) # update the k-vector, to zero for each new trial of the shooting 
+k_temp[1]=kbar
+c_temp=  zeros(m.n)
+c_temp[1] = c0 
+uc_temp = zeros(m.n) 
+uc_temp[1]=m.deru(c0,m.p)
+for j in 1:m.n-1
+lawofmotion_k(m, c_temp, k_temp, j)  	##Compute next period capital 
+eulerequation(m, uc_temp, k_temp, j)	##Compute next period marg.utility
+c_temp[j+1] = invderu(uc_temp[j+1],m.p)		## inverse to get the nextperiod consumption
+#uc[i] = m.deru(c[i])
+end
+ks = k_temp[end]
+cs = c_temp[end]
+
+## only return the last period consumption and capital
+return ks, cs
+
+end
+
+
+
+function loop_c_k_path(m::Model, c0::Real, kbar::Real)
+### the same loop, but return the path of the capital and consumption
+
+k_temp = zeros(m.n) # update the k-vector, to zero for each new trial of the shooting 
+k_temp[1]=kbar
+c_temp=  zeros(m.n)
+c_temp[1] = c0 
+uc_temp = zeros(m.n) 
+uc_temp[1]=m.deru(c0,m.p)
+for j in 1:m.n-1
+lawofmotion_k(m, c_temp, k_temp, j)  	##Compute next period capital 
+eulerequation(m, uc_temp, k_temp, j)	##Compute next period marg.utility
+c_temp[j+1] = invderu(uc_temp[j+1],m.p)		## inverse to get the nextperiod consumption
+#uc[i] = m.deru(c[i])
+end
+k_path = k_temp
+c_path = c_temp
+
+return k_path, c_path
+end
+
+
+
+	function shootingalgorithm(m::Model)
+
+steadystate(m)
+kbar = m.endo.k[end]
+
+#################################################
+############################ 1st scale
+
+## Draw a grid at the 1e-3 level
+c0bis=[1e-3n for n in 1:1000]
+kbar_bis = zeros(1000)
+cbar_bis = zeros(1000)
+
+	for index in 1:1000
+c0=c0bis[index]
+# compute the 1000-paths of consumption and capital drawn from the loop of the model. 
+result = loop_c_k(m, c0, kbar)
+kbar_bis[index] = result[1]
+cbar_bis[index] = result[2]
+	end
+
+### find the index (among the 1000) which gives a final capital close to the steady state value
+kdiff = kbar_bis - kbar*ones(1000)
+ind_temp=findmin(abs(kdiff))[2] ## find its index
+if kdiff[ind_temp] > 0
+ind = ind_temp
+elseif kdiff[ind_temp] <0
+ind = ind_temp-1
+end 
+##OR
+#ind = findmax(cbar_bis)[2] ## find the index where the consumption is not null (slightly faster)
+c1 = 1e-3*ind
+c1ter=round(c1, 5)
+ks = kbar_bis[ind]
+
+
+#################################################
+############################ 2nd scale
+#### Draw a grid at the 1e-6 level
+
+c1bis = [c1ter+1e-6n for n in 1:1000]
+kbar_ter = zeros(1000)
+cbar_ter = zeros(1000)
+
+	for index2 in 1:1000
+c1=c1bis[index2]
+# again, compute the 1000-paths of consumption and capital drawn from the loop of the model
+# (with difference in initial condition much smaller than in the loop above). 
+result= loop_c_k(m, c1, kbar)
+kbar_ter[index2] = result[1]
+cbar_ter[index2] = result[2]
+	end 
+
+## Find the "shoot" giving the target-capital the closest to the steady-state value. 
+kdiff = kbar_ter - kbar*ones(1000)
+ind_temp=findmin(abs(kdiff))[2] ## find its index
+
+# you want to take the "shoot", for the value yielding a Ks superior to Kbar
+if kdiff[ind_temp] > 0
+ind2 = ind_temp
+elseif kdiff[ind_temp] <0
+ind2 = ind_temp-1
+end 
+c2ter = c1ter+1e-6*ind2 ## compute the closest initial consumption level
+ks= kbar_ter[ind2]
+
+#################################################
+############################ 3rd scale
+######### Draw a grid at the 1e-9 level
+
+c2bis = [c2ter+1e-9n for n in 1:1000]
+kbar_quar = zeros(1000)
+cbar_quar = zeros(1000)
+
+	for index3 in 1:1000
+c2=c2bis[index3]
+# again, compute the 1000-paths of consumption and capital drawn from the loop of the model
+# (with difference in initial condition again much smaller than in the loop above). 
+result= loop_c_k(m, c2, kbar)
+kbar_quar[index3] = result[1]
+cbar_quar[index3] = result[2]
+	end 
+
+## Find the "shoot" giving the target-capital the closest to the steady-state value. 
+kdiff = kbar_quar - kbar*ones(1000)
+ind_temp=findmin(abs(kdiff))[2] ## find its index
+
+# you want to take the "shoot", for the value yielding a Ks superior to Kbar
+if kdiff[ind_temp] > 0
+ind3 = ind_temp
+elseif kdiff[ind_temp] <0
+ind3 = ind_temp-1
+end 
+c3ter = c2ter+1e-9*ind3  ## compute the closest initial consumption level
+ks= kbar_quar[ind3]
+
+
+#################################################
+############################ 4th scale
+########## Draw a grid at the 1e-12 level
+
+c3bis = [c3ter+1e-12n for n in 1:1000]
+kbar_quin = zeros(1000)
+cbar_quin = zeros(1000)
+
+	for index4 in 1:1000
+c3=c3bis[index4]
+# again, compute the 1000-paths of consumption and capital drawn from the loop of the model
+# (with difference in initial condition again much smaller than in the loop above). 
+result= loop_c_k(m, c3, kbar)
+kbar_quin[index4] = result[1]
+cbar_quin[index4] = result[2]
+	end 
+
+## Find the "shoot" giving the target-capital the closest to the steady-state value. 
+kdiff = kbar_quin - kbar*ones(1000)
+ind_temp=findmin(abs(kdiff))[2] ## find its index
+
+# you want to take the "shoot", for the value yielding a Ks superior to Kbar
+if kdiff[ind_temp] > 0
+ind4 = ind_temp
+elseif kdiff[ind_temp] <0
+ind4 = ind_temp-1
+end 
+c4ter = c3ter+1e-12*ind4  ## compute the closest initial consumption level
+ks= kbar_quin[ind4]
+
+## Results and display a small message after the result
+
+c0 = c4ter
+ks = kbar_quin[ind4]
+
+signif=0.0
+for i in 1:9
+if abs(kbar-ks) < 10.0^(-i)
+signif = 10.0^(-i)
+end 
+end
+
+println("The shooting algorithm needs 3 loops (at 4 different scales (1e-3, 1e-6, 1e-9, 1e-12) to get the right c0 = $c0")
+println("The obtained final capital is Ks = $ks, which is close to the true kbar = $kbar at the $signif level")
+return c0, ks, kbar, signif
+end
+
+### Compute the rest. 
+function model_path(m::Model, tol::Real=1e-6)
+
+shooting = shootingalgorithm(m)
+
+c0 = shooting[1]
+ks = shooting[2]
+kbar = shooting[3]
+
+
+	if abs(ks - kbar) < tol 
+	# it should be if the loop-above is correct (test it?)
+	## assign the new quantities & compute the equilibrium
+k_temp = zeros(m.n)
+c_temp = zeros(m.n)
+path= loop_c_k_path(m, c0, kbar)
+k_temp = path[1]	
+c_temp = path[2]
+equilibriumquantities(m, c_temp, k_temp)
+
+	end 
+
+end
+
+				end 
+
---
+++ src/Policy_experiment.jl
@@ -0,0 +1,465 @@
+
+
+module Policy_experiment
+
+############################################ Preliminary
+############################################
+### Try the normal steady state value, without policy experiment. 
+
+g = 0.2*ones(20)
+tauk=zeros(20)
+tauc=zeros(20)
+taun=zeros(20)
+
+p=Parameter()
+exo=exovariable(g,tauk,tauc,taun,p)
+endo=endovariable(exo)
+m=Imp_model(exo, endo, u, deru, invderu, f, derf, p)
+
+#Modify all the variables of the model, following the exogenous variations. 
+model_path(m, 1e-4)
+
+
+############################################
+### Plot of convergence of the shooting algorithm
+shooting = shootingalgorithm(m)
+
+c0 = shooting[1]
+ks = shooting[2]
+kbar = shooting[3]
+
+c0_below=[0.1, 0.5, 0.6, 0.64, (1-1e-3)c0, (1-1e-4)c0, (1-1e-5)c0, (1-5*1e-6)c0, (1-4*1e-6)c0, (1-3*1e-6)c0, (1-2*1e-6)c0,  (1-1e-6)c0, (1-5*1e-7)c0, (1-2*1e-7)c0, (1-1e-7)c0, (1-5*1e-8)c0,(1-4*1e-8)c0, (1-3*1e-8)c0, (1-2*1e-8)c0, (1-1e-8)c0]
+
+# reminder truec0 = 0.64264525131
+x=[n for n in 1:m.n] ## periods of time
+#plot(x, m.endo.k)
+
+kbarvector = kbar*ones(m.n)
+cbarvector = 0.6426452513109608*ones(m.n)
+
+fig, axes = subplots(2,2)
+#    ax[:set_ylim](0, 10)
+#    ax[:set_xlim](, maximum(grid))
+#    lb = "Initial condition"
+    jet = ColorMap("jet")[:__call__]
+    fig[:suptitle]("Shooting algorithm, convergence from below the steady state level")
+    ax = axes[1,1]
+    ax[:plot](x, kbarvector, color=jet(0))
+    ax[:set_title]("Convergence, capital")
+    ax = axes[2,1]
+    ax[:plot](x, kbarvector, color=jet(0))
+	ax[:set_title]("Capital path, zoom")
+
+	ax = axes[1,2]
+    ax[:plot](x, cbarvector, color=jet(0))
+    ax[:set_title]("Convergence, consumption")
+    ax = axes[2,2]
+    ax[:plot](x, cbarvector, color=jet(0))
+    ax[:set_title]("Consumption path, zoom")
+# lw = 2 lign width
+# alpha = transparence
+# jet = ColorMap("jet")[:__call__] (color of the jet() 0 if 
+
+	for index in 1:length(c0_below)
+c0=c0_below[index]
+path= loop_c_k_path(m, c0, kbar)
+k_temp = path[1]	
+c_temp = path[2]
+ax = axes[1,1]
+ax[:plot](x, k_temp, color=jet(index/length(c0_below)))
+
+if index > 9 
+ax = axes[2,1]
+ax[:plot](x, k_temp, color=jet(index/length(c0_below)))
+end 
+
+ax = axes[1,2]
+ax[:plot](x, c_temp, color=jet(index/length(c0_below)))
+
+if index > 9 
+ax = axes[2,2]
+ax[:plot](x, c_temp, color=jet(index/length(c0_below)))
+end 
+	end
+#ax[:legend](loc="upper left")
+
+############################################
+### From above
+c0_above=[0.95, 0.8, 0.7, 0.65, (1+1e-3)c0, (1+1e-4)c0, (1+1e-5)c0, (1+5*1e-6)c0, (1+4*1e-6)c0, (1+3*1e-6)c0, (1+2*1e-6)c0,  (1+1e-6)c0, (1+5*1e-7)c0, (1+2*1e-7)c0, (1+1e-7)c0, (1+5*1e-8)c0,(1+4*1e-8)c0, (1+3*1e-8)c0, (1+2*1e-8)c0, (1+1e-8)c0]
+
+# reminder truec0 = 0.64264525131
+x=[n for n in 1:m.n] ## periods of time
+#plot(x, m.endo.k)
+
+kbarvector = kbar*ones(m.n)
+cbarvector = 0.6426452513109608*ones(m.n)
+
+fig, axes = subplots(2,2)
+#    ax[:set_ylim](0, 10)
+#    ax[:set_xlim](, maximum(grid))
+#    lb = "Initial condition"
+    jet = ColorMap("jet")[:__call__]
+     fig[:suptitle]("Shooting algorithm, convergence from above the steady state level")
+    ax = axes[1,1]
+    ax[:plot](x, kbarvector, color=jet(0))
+    ax[:set_title]("Convergence, capital")
+    ax = axes[2,1]
+    ax[:plot](x, kbarvector, color=jet(0))
+	ax[:set_title]("Capital path, zoom")
+
+	ax = axes[1,2]
+    ax[:plot](x, cbarvector, color=jet(0))
+    ax[:set_title]("Convergence, consumption")
+    ax = axes[2,2]
+    ax[:plot](x, cbarvector, color=jet(0))
+    ax[:set_title]("Consumption path, zoom")
+# lw = 2 lign width
+# alpha = transparence
+# jet = ColorMap("jet")[:__call__] (color of the jet() 0 if 
+
+	for index in 1:length(c0_above)
+c0=c0_above[index]
+path= loop_c_k_path(m, c0, kbar)
+k_temp = path[1]	
+c_temp = path[2]
+ax = axes[1,1]
+ax[:plot](x, k_temp, color=jet(index/length(c0_above)))
+
+if index > 9 
+ax = axes[2,1]
+ax[:plot](x, k_temp, color=jet(index/length(c0_above)))
+end 
+
+ax = axes[1,2]
+ax[:plot](x, c_temp, color=jet(index/length(c0_above)))
+
+if index > 9 
+ax = axes[2,2]
+ax[:plot](x, c_temp, color=jet(index/length(c0_above)))
+end
+
+	end
+
+
+############################################
+#### Plot the IRF, for the usual variables 
+
+
+g = 0.2*ones(20)
+tauk=zeros(20)
+tauc=zeros(20)
+taun=zeros(20)
+
+p=Parameter()
+exo=exovariable(g,tauk,tauc,taun,p)
+endo=endovariable(exo)
+m=Imp_model(exo, endo, u, deru, invderu, f, derf, p)
+
+#Modify all the variables of the model, following the exogenous variations. 
+model_path(m, 1e-4)
+
+mref=Imp_model(exo, endo, u, deru, invderu, f, derf, p)
+
+
+fprod=zeros(m.n)
+for j in 1:m.n
+fprod[j]=mref.f(m.endo.k[j], m.p)
+end
+
+x=[n for n in 1:m.n]
+plot(x, mref.endo.k)
+plot(x, mref.endo.c)
+plot(x, mref.exo.g)
+plot(x, fprod)
+
+
+fig,axes = subplots(2,3,figsize=(10,5))
+
+ax = axes[1,1]
+ax[:plot](x, m.endo.k)
+ax[:plot](x, zeros(m.n))
+ax = axes[1,2]
+ax[:plot](x, m.endo.c)
+ax[:plot](x, zeros(m.n))
+ax = axes[1,3]
+ax[:plot](x, m.exo.g)
+ax[:plot](x, zeros(m.n))
+ax = axes[2,1]
+ax[:plot](x, fprod)
+ax[:plot](x, zeros(m.n))
+ax = axes[2,2]
+ax[:plot](x, m.endo.R)
+ax[:plot](x, zeros(m.n))
+ax = axes[2,3]
+ax[:plot](x, m.endo.eta)
+ax[:plot](x, zeros(m.n))
+
+fig[:canvas][:draw]()
+
+
+
+############################################ Second part, 
+############################################ First policy shock
+## Second, increase once-for-all in g at period 10. 
+
+tauk=zeros(20)
+tauc=zeros(20)
+taun=zeros(20)
+g = [0.2*ones(10);0.4*ones(10)]
+
+p=Parameter()
+exo=exovariable(g,tauk,tauc,taun,p)
+endo=endovariable(exo)
+m=Imp_model(exo, endo, u, deru, invderu, f, derf, p)
+model_path(m, 1e-4)
+
+
+
+x=[n for n in 1:m.n]
+plot(x, m.endo.k)
+plot(x, m.endo.c)
+plot(x, m.exo.g)
+
+
+
+fig,axes = subplots(2,3,figsize=(10,5))
+
+ax = axes[1,1]
+ax[:plot](x, m.endo.k)
+#ax[:plot](x, zeros(m.n))
+ax = axes[1,2]
+ax[:plot](x, m.endo.c)
+#ax[:plot](x, zeros(m.n))
+ax = axes[1,3]
+ax[:plot](x, m.exo.g)
+#ax[:plot](x, zeros(m.n))
+ax = axes[2,1]
+ax[:plot](x, fprod)
+#ax[:plot](x, zeros(m.n))
+ax = axes[2,2]
+ax[:plot](x, m.endo.R)
+#ax[:plot](x, zeros(m.n))
+ax = axes[2,3]
+ax[:plot](x, m.endo.eta)
+#ax[:plot](x, zeros(m.n))
+
+fig[:canvas][:draw]()
+
+
+
+
+
+############################################ Third part, 
+############################################ second policy shock
+## Third, increase once-for-all the consumption tax at period 10. 
+
+
+
+tauk=zeros(20)
+tauc=[zeros(10);0.2*ones(10)]
+taun=zeros(20)
+g = 0.2*ones(20)
+
+
+p=Parameter()
+exo=exovariable(g,tauk,tauc,taun,p)
+endo=endovariable(exo)
+m=Imp_model(exo, endo, u, deru, invderu, f, derf, p)
+
+model_path(m, 1e-4)
+
+
+fig,axes = subplots(2,3,figsize=(10,5))
+
+ax = axes[1,1]
+ax[:plot](x, m.endo.k)
+#ax[:plot](x, zeros(m.n))
+ax = axes[1,2]
+ax[:plot](x, m.endo.c)
+#ax[:plot](x, zeros(m.n))
+ax = axes[1,3]
+ax[:plot](x, m.exo.tauc)
+#ax[:plot](x, zeros(m.n))
+ax = axes[2,1]
+ax[:plot](x, fprod)
+#ax[:plot](x, zeros(m.n))
+ax = axes[2,2]
+ax[:plot](x, m.endo.R)
+#ax[:plot](x, zeros(m.n))
+ax = axes[2,3]
+ax[:plot](x, m.endo.eta)
+#ax[:plot](x, zeros(m.n))
+
+fig[:canvas][:draw]()
+
+
+
+
+
+
+############################################ Fourth part, 
+############################################ Third policy shock
+## Fourth, increase once-for-all the capital tax at period 10. 
+
+tauc=zeros(20)
+tauk=[zeros(10);0.2*ones(10)]
+taun=zeros(20)
+g = 0.2*ones(20)
+
+
+p=Parameter()
+exo=exovariable(g,tauk,tauc,taun,p)
+endo=endovariable(exo)
+m=Imp_model(exo, endo, u, deru, invderu, f, derf, p)
+
+model_path(m, 1e-4)
+
+
+fig,axes = subplots(2,3,figsize=(10,5))
+
+ax = axes[1,1]
+ax[:plot](x, m.endo.k)
+#ax[:plot](x, zeros(m.n))
+ax = axes[1,2]
+ax[:plot](x, m.endo.c)
+#ax[:plot](x, zeros(m.n))
+ax = axes[1,3]
+ax[:plot](x, m.exo.tauc)
+#ax[:plot](x, zeros(m.n))
+ax = axes[2,1]
+ax[:plot](x, fprod)
+#ax[:plot](x, zeros(m.n))
+ax = axes[2,2]
+ax[:plot](x, m.endo.R)
+#ax[:plot](x, zeros(m.n))
+ax = axes[2,3]
+ax[:plot](x, m.endo.eta)
+#ax[:plot](x, zeros(m.n))
+
+fig[:canvas][:draw]()
+
+
+
+
+
+
+############################################ Fifth part, 
+############################################ Fourth policy shock
+## Fifth, increase, one time-pulse of government spending at period 10
+
+
+tauk=zeros(20)
+tauc=zeros(20)
+taun=zeros(20)
+g = 0.2*ones(20)
+g[10] = 0.4
+
+p=Parameter()
+exo=exovariable(g,tauk,tauc,taun,p)
+endo=endovariable(exo)
+m=Imp_model(exo, endo, u, deru, invderu, f, derf, p)
+model_path(m, 1e-4)
+
+
+
+x=[n for n in 1:m.n]
+plot(x, m.endo.k)
+plot(x, m.endo.c)
+plot(x, m.exo.g)
+
+
+
+fig,axes = subplots(2,3,figsize=(10,5))
+
+ax = axes[1,1]
+ax[:plot](x, m.endo.k)
+#ax[:plot](x, zeros(m.n))
+ax = axes[1,2]
+ax[:plot](x, m.endo.c)
+#ax[:plot](x, zeros(m.n))
+ax = axes[1,3]
+ax[:plot](x, m.exo.g)
+#ax[:plot](x, zeros(m.n))
+ax = axes[2,1]
+ax[:plot](x, fprod)
+#ax[:plot](x, zeros(m.n))
+ax = axes[2,2]
+ax[:plot](x, m.endo.R)
+#ax[:plot](x, zeros(m.n))
+ax = axes[2,3]
+ax[:plot](x, m.endo.eta)
+#ax[:plot](x, zeros(m.n))
+
+fig[:canvas][:draw]()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#### other drafts, to be cleaned
+
+y=[0.001n for n in 1:1000]
+
+
+
+# look at the path of capital
+
+
+x=[n for n in 1:m.n]
+
+fk=zeros(m.n)
+for j in 1:m.n
+fk[j]=m.derf(k_temp[j], m.p)
+end
+
+
+
+c_temp3 = m.f(kbar, m.p) + (1-m.p["delta"])*kbar -kbar - g[end]
+
+c_temp2 = zeros(m.n)
+for j in 1:m.n-1
+c_temp2[j]=m.f(k_temp[j], m.p) + (1-m.p["delta"])*k_temp[j] -k_temp[j+1] -g[j]
+end
+
+invest = zeros(m.n)
+for j in 1:m.n-1
+invest[j]=k_temp[j+1] - (1-m.p["delta"])*k_temp[j]
+end
+
+
+
+#fig,axes = subplots(1,4,figsize=(10,5))
+#ax = axes[1,1]
+plot(x, k_temp)
+plot(x, c_temp)
+#plot(x, c_temp2)
+plot(x, uc_temp)
+plot(x, fk)
+
+
+plot(x, fkap)
+plot(x, g)
+plot(x, invest)
+plot(x, c_temp)
+
+
+ax = axes[1,2]
+ax[:plot](x, c_temp)
+ax = axes[1,3]
+axes[:plot](x, uc_temp)
+
+ax = axes[1,4]
+axes[:plot](x, fk)
+
+
+end 
